cf297fa34cd5a02f2dfc9065a64aec056e13ca68
AppleBee12 <mieldeluna@naver.com>
2024-12-02 17:46:19 +0900

    📝 : title 추가

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <?php
       if ( have_posts() ) :
         while ( have_posts() ) : the_post();
+            echo "<h2> the_title</h2>()";
             the_content();
         endwhile;
       else :
